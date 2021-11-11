5ace8178f24525aa52b58fb3f7b433ac27b08ed1
acm <alexis_cm@live.com>
2021-11-11 02:49:37 -0800

    Added basic data model for Items and Category, main activity

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# GoGoGroceries
+# Go Go Groceries
+
+
+
+## Testing
+
+* Android 11, API 29 (Q)
+* Virtual Device: Pixel 3 API 29
+
---
+++ app/src/main/java/com/comp7082/gogogroceries/Item.java
@@ -0,0 +1,59 @@
+package com.comp7082.gogogroceries;
+
+import java.util.Date;
+
+public class Item {
+    private String _name;
+    private Category _category;
+    private Date _expiryDate;
+    private boolean _isReoccurring;
+    private String _note;
+
+    public Item(String name, Category cat, Date expiryDate, boolean isReoccurring, String note) {
+        _name = name;
+        _category = cat;
+        _expiryDate = expiryDate;
+        _isReoccurring = isReoccurring;
+        _note = note;
+    }
+
+    public void setName(String newName) {
+        _name = newName;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setCategory(Category newCat) {
+        _category = newCat;
+    }
+
+    public Category getCategory() {
+        return _category;
+    }
+
+    public void setExpiryDate(Date newExpiry) {
+        _expiryDate = newExpiry;
+    }
+
+    public Date getExpiryDate() {
+        return _expiryDate;
+    }
+
+    public void setReoccurring(boolean isReoccurring) {
+        _isReoccurring = isReoccurring;
+    }
+
+    public boolean getIsReoccurring() {
+        return _isReoccurring;
+    }
+
+    public void setNote(String newNote) {
+        _note = newNote;
+    }
+
+    public String getNote() {
+        return _note;
+    }
+}
